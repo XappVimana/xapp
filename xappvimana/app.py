833626e25aedab477d1f3246eacbe10adc04fe9c
XappVimana <61242186+XappVimana@users.noreply.github.com>
2020-02-20 22:44:24 +0530

    Add files via upload

--- xappvimana/app.py
+++ xappvimana/app.py
@@ -1,17 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash, app
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-
-@app.route("/about")
+@app.route('/', methods=['GET', 'POST'])
 def about():
-    return render_template("about.html")
+    if request.method == 'POST':
+        if 'Projects' in request.form:
+            return redirect(url_for('projects'))
+        elif 'Name' in request.form:
+            return redirect(url_for('name'))
+
+        return render_template('About.html')
 
 
-if __name__ == "__main__":
+    else:
+
+        return render_template('About.html')
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    if request.method == 'POST':
+        if 'Projects' in request.form:
+            return redirect(url_for('projects'))
+        elif 'About' in request.form:
+            return redirect(url_for('about'))
+
+        print('lalalal')
+        details = request.form
+        # amez = details['name']
+        # namez=Names(name=namez,)
+        # db.session.add(namez)
+        # db.session.commit()
+
+        return render_template('Names.html', name=[])
+
+
+    else:
+
+        return render_template('Names.html', name=[])
+
+
+@app.route('/projects', methods=['GET', 'POST'])
+def name():
+    if request.method == 'POST':
+        if 'Name' in request.form:
+            return redirect(url_for('name'))
+        elif 'About' in request.form:
+            return redirect(url_for('about'))
+
+        return render_template('Projects.html')
+
+
+    else:
+        return render_template('Projects.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
